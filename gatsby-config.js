2a0202f11856bb6dd8cc4d51bf0454438053ad17
MitchelPaulin <mitchel0022@gmail.com>
2021-04-25 12:39:30 -0400

    Changed project root

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,5 +41,5 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-  pathPrefix: "/goldengoblet",
+  pathPrefix: "/goldengoblet.net/",
 }
